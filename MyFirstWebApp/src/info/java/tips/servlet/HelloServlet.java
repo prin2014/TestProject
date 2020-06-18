2231b47cfb74794a8d42ec0e32a6f7310e4560b9
Priyojit <Priyojit@DESKTOP-KK3TCAN>
2020-06-18 15:21:54 +0530

    First commit

---
+++ MyFirstWebApp/src/info/java/tips/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package info.java.tips.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		out.println("LOLOLOLOL");
+	}
+
+}
